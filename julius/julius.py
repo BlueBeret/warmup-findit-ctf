aa350e439050d27b446382d4dab90851c7c70088
BlueBeret <63086194+BlueBeret@users.noreply.github.com>
2022-05-07 16:14:12 +0700

    initial

---
+++ julius/julius.py
@@ -0,0 +1,12 @@
+""""
+Julius sends us a message: ?bg]BM<M?tP^eeXA^eehXfrX_kb,g].v
+
+But we can't understand it. He then responded, "7 shift" We still can't understand it. He then responded again, "ASCII code"
+
+
+"""
+
+c = '?bg]BM<M?tP^eeXA^eehXfrX_kb,g].v'
+
+for i in c :
+    print(chr(ord(i)+7), end="")
